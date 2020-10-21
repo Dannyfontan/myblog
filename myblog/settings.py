aebf8584a083c2deb5a4fa6c20ecec428e3a66e1
danny <danny141024@gmail.com>
2020-10-21 10:32:35 +0800

    add ICP

--- myblog/settings.py
+++ myblog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/danny/dj_project/myblog/static/'
+#STATIC_ROOT = '/home/danny/dj_project/myblog/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )

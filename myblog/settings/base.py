4fb7d563a4744e506d02b8e9992f2e8d446e3421
danny <danny141024@gmail.com>
2020-10-26 22:04:34 +0800

    add settings folder

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -13,20 +13,16 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!(r!mp4t--5l@t@wqat89b-=0xcpi8q*=9*0(6h^8%%mx#$9-z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -38,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
-    'userprofile',
     'mdeditor',
 ]
 
@@ -120,8 +115,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-#)
---
+++ myblog/settings/development.py
@@ -0,0 +1,9 @@
+from .base import *
+print("wadwadwadawdawd")
+DEBUG = True
+
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)

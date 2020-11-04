08be29926a38c8c5141d4900b05c5ea944049a16
danny <ddanny141024@gmail.com>
2020-11-04 21:37:30 +0800

    rm docker file

--- myblog/settings/development.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-print("wadwadwadawdawd")
-DEBUG = True
-
-STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
